29caa8212c178a26b484bfa4100a091728677505
sudo-rushil <rushil.mallarapu@gmail.com>
2020-07-03 22:25:27 -0400

    finished basic evaluation

--- src/Evaluator.hs
+++ src/Evaluator.hs
@@ -15,6 +15,63 @@ eval val@(Float _)              = val
 eval val@(Ratio _)              = val
 eval val@(Complex _)            = val
 eval (List [Atom "quote", val]) = val
+eval (List (Atom func : args))  = apply func $ map eval args -- function application
+
+
+apply :: String -> [LispVal] -> LispVal
+apply func args = maybe (Bool False) ($ args) $ lookup func primitives
+
+
+primitives :: [(String, [LispVal] -> LispVal)]
+primitives =
+    [ ("+", numericBinop (+))
+    , ("-", numericBinop (-))
+    , ("*", numericBinop (*))
+    , ("/", numericBinop div)
+    , ("mod", numericBinop mod)
+    , ("quotient", numericBinop quot)
+    , ("remainder", numericBinop rem)
+    , ("symbol?", unaryOp symbolp)
+    , ("string?", unaryOp stringp)
+    , ("number?", unaryOp numberp)
+    , ("bool?", unaryOp boolp)
+    , ("list?", unaryOp listp)
+    , ("symbol->string", unaryOp symbol2string)
+    , ("string->symbol", unaryOp string2symbol)
+    ]
+
+
+numericBinop :: (Integer -> Integer -> Integer) -> [LispVal] -> LispVal
+numericBinop op params = Number $ foldl1 op $ map unpackNum params
+
+
+unpackNum :: LispVal -> Integer
+unpackNum (Number n) = n
+unpackNum _          = 0
+
+
+unaryOp :: (LispVal -> LispVal) -> [LispVal] -> LispVal
+unaryOp f [v] = f v -- error to be given more than 2 args
+
+
+symbolp, stringp, numberp, boolp, listp :: LispVal -> LispVal
+symbolp (Atom _) = Bool True
+symbolp _        = Bool False
+stringp (String _) = Bool True
+stringp _          = Bool False
+numberp (Number _) = Bool True
+numberp _          = Bool False
+boolp (Bool _) = Bool True
+boolp _        = Bool False
+listp (List _) = Bool True
+listp _        = Bool False
+
+
+symbol2string, string2symbol :: LispVal -> LispVal
+symbol2string (Atom s) = String s
+symbol2string _        = String ""
+string2symbol (String s) = Atom s
+string2symbol _          = Atom ""
 
 
 -- Instances
