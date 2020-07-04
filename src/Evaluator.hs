1c7aa0578b51c5e3e57cefbd49bc70d6c56acd5d
sudo-rushil <rushil.mallarapu@gmail.com>
2020-07-04 01:16:55 -0400

    expanding exercise

--- src/Evaluator.hs
+++ src/Evaluator.hs
@@ -1,12 +1,15 @@
+{-# LANGUAGE ExistentialQuantification #-}
+
 module Evaluator where
 
 
+import           Control.Monad                 (liftM)
 import           Control.Monad.Except
 import           Parser
 import           Text.ParserCombinators.Parsec (ParseError)
 
 
--- Exception Types
+-- Types
 
 data LispError = NumArgs Integer [LispVal]
     | TypeMismatch String LispVal
@@ -19,6 +22,8 @@ data LispError = NumArgs Integer [LispVal]
 
 type ThrowsError = Either LispError
 
+
+data Unpacker = forall a . Eq a => AnyUnpacker (LispVal -> ThrowsError a)
 
 -- Error Handling Functions
 
@@ -45,8 +50,9 @@ eval (List [Atom "if", pred, conseq, alt]) = -- if statement
     do
         result <- eval pred
         case result of
+            Bool True  -> eval conseq
             Bool False -> eval alt
-            _          -> eval conseq
+            notBool    -> throwError $ TypeMismatch "boolean" notBool
 eval (List (Atom func : args))  = mapM eval args >>= apply func -- function application
 eval badForm = throwError $ BadSpecialForm "Unrecognized special form" badForm
 
@@ -84,6 +90,12 @@ primitives =
     , ("string>?", strBoolBinop (>))
     , ("string<=?", strBoolBinop (<=))
     , ("string>=?", strBoolBinop (>=))
+    , ("car", car)
+    , ("cdr", cdr)
+    , ("cons", cons)
+    , ("eq?", eqv)
+    , ("eqv?", eqv)
+    , ("equal?", equal)
     ]
 
 
@@ -155,6 +167,71 @@ unpackStr notString  = throwError $ TypeMismatch "string" notString
 unpackBool :: LispVal -> ThrowsError Bool
 unpackBool (Bool b) = return b
 unpackBool notBool  = throwError $ TypeMismatch "boolean" notBool
+
+
+-- List Primitives
+
+car :: [LispVal] -> ThrowsError LispVal
+car [List (x:xs)]         = return x
+car [DottedList (x:xs) _] = return x
+car [badArg]              = throwError $ TypeMismatch "pair" badArg
+car badArgList            = throwError $ NumArgs 1 badArgList
+
+
+cdr :: [LispVal] -> ThrowsError LispVal
+cdr [List (x:xs)]           = return $ List xs
+cdr [DottedList [_] x]      = return x
+cdr [DottedList (_ : xs) x] = return $ DottedList xs x
+cdr [badArg]                = throwError $ TypeMismatch "pair" badArg
+cdr badArgList              = throwError $ NumArgs 1 badArgList
+
+
+cons :: [LispVal] -> ThrowsError LispVal
+cons [x1, List []]            = return $ List [x1]
+cons [x, List xs]             = return $ List $ x : xs
+cons [x, DottedList xs xlast] = return $ DottedList (x : xs) xlast
+cons [x1, x2]                 = return $ DottedList [x1] x2
+cons badArgList               = throwError $ NumArgs 2 badArgList
+
+
+-- Equivalence Predicates
+
+eqv :: [LispVal] -> ThrowsError LispVal
+eqv [(Bool arg1), (Bool arg2)] = return $ Bool $ arg1 == arg2
+eqv [(Number arg1), (Number arg2)] = return $ Bool $ arg1 == arg2
+eqv [(String arg1), (String arg2)] = return $ Bool $ arg1 == arg2
+eqv [(Atom arg1), (Atom arg2)] = return $ Bool $ arg1 == arg2
+eqv [(DottedList xs x), (DottedList ys y)] = eqv [List $ xs ++ [x], List $ ys ++ [y]]
+eqv listPair@[(List _), (List _)] = eqvList eqv listPair
+eqv [_, _] = return $ Bool False
+eqv badArgList = throwError $ NumArgs 2 badArgList
+
+
+equal :: [LispVal] -> ThrowsError LispVal
+equal listPair@[(List _), (List _)] = eqvList equal listPair
+equal [(DottedList xs x), (DottedList ys y)] = equal [List $ xs ++ [x], List $ ys ++ [y]]
+equal [arg1, arg2] = do
+        primitiveEquals <- liftM or $ mapM (unpackEquals arg1 arg2) [AnyUnpacker unpackNum, AnyUnpacker unpackStr, AnyUnpacker unpackBool]
+        eqvEquals <- eqv [arg1, arg2]
+        return $ Bool $ (primitiveEquals || let (Bool x) = eqvEquals in x)
+equal badArgList = throwError $ NumArgs 2 badArgList
+
+
+unpackEquals :: LispVal -> LispVal -> Unpacker -> ThrowsError Bool
+unpackEquals arg1 arg2 (AnyUnpacker unpacker) =
+    do
+        unpacked1 <- unpacker arg1
+        unpacked2 <- unpacker  arg2
+        return $ unpacked1 == unpacked2
+    `catchError` (const $ return False)
+
+
+eqvList :: ([LispVal] -> ThrowsError LispVal) -> [LispVal] -> ThrowsError LispVal
+eqvList eqvFunc [(List arg1), (List arg2)] = return $ Bool $ (length arg1 == length arg2) && (all eqvPair $ zip arg1 arg2)
+    where
+        eqvPair (x1, x2) = case eqvFunc [x1, x2] of
+            Left err         -> False
+            Right (Bool val) -> val
 
 
 -- Instances
